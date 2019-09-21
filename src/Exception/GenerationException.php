81bf38b5436ffb4d6de6853cf560db954a1d21d2
Turenar <eclair@turenar.xyz>
2019-09-21 14:55:40 +0900

    change base type of GenerationException to RuntimeException

--- src/Exception/GenerationException.php
+++ src/Exception/GenerationException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 namespace Turenar\ApiSchema\Exception;
 
 
-abstract class GenerationException extends \Exception
+abstract class GenerationException extends \RuntimeException
 {
 }
